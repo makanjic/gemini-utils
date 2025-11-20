14169421a9c28f2c6c2f9f83fbc0d27b78926b87
Makanja <makanjicdusan313@outlook.com>
2025-11-20 14:45:06 +0900

    Create google_chat.py

---
+++ google_chat.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+from google import genai
+
+client = genai.Client()
+
+response = client.models.generate_content(
+    model="gemini-2.5-flash", contents="Explain how AI works in a few words"
+)
+print(response.text)
