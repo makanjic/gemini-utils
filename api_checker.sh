2ee2cbddb69e7dd9845956943c28c7e9aff83ce1
Makanja <makanjicdusan313@outlook.com>
2025-11-20 14:50:38 +0900

    Change script to use google_chat.py instead of chat.py

--- api_checker.sh
+++ api_checker.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
 
+WORKDIR=$(readlink -f $(dirname $0))
+cd $WORKDIR
+
 source .venv/bin/activate
 
 while read key; do
 	export GEMINI_API_KEY=$key
-	python3 chat.py
+	python3 google_chat.py
 	if [ $? -eq 0 ]; then
 		echo SUCCESS $key
 	else
 		echo FAILURE $key
 	fi
-	export GEMINI_API_KEY=
+	export GEMINI_API_KEY=""
 	unset GEMINI_API_KEY
 done
